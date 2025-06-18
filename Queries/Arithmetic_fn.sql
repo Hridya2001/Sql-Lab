dd83ad7ef2a8525de9f7e9bd777665cf6e629569
Hridya Umesh <hridyaumesh2@gmail.com>
2025-06-18 16:13:35 +0530

    Update Arithmetic_fn.sql

--- Queries/Arithmetic_fn.sql
+++ Queries/Arithmetic_fn.sql
@@ -1,39 +1,19 @@
 -- 1. Add ₹5000 bonus to each employee's salary
-SELECT 
-    first_name,
-    salary,
-    salary + 5000 AS increased_salary
-FROM employees;
+select first_name,salary,salary + 5000 as increased_salary from employees;
 
 
 -- 2. Deduct ₹2000 from each employee's salary
-SELECT 
-    first_name,
-    salary,
-    salary - 2000 AS adjusted_salary
-FROM employees;
+select first_name, salary,salary - 2000 as adjusted_salary from employees;
 
 
 -- 3, Calculate annual salary assuming salary is monthly
-SELECT 
-    first_name,
-    salary,
-    salary * 12 AS annual_salary
-FROM employees;
+select first_name,salary, salary * 12 as annual_salary from employees;
 
 
 -- 4. Divide salary by 2 (maybe to calculate part-time pay)
-SELECT 
-    first_name,
-    salary,
-    salary / 2 AS half_salary
-FROM employees;
+select first_name,salary,salary / 2 as half_salary from employees;
 
 
 -- 5. Find employees whose salary is an exact multiple of ₹10000
-SELECT 
-    first_name,
-    salary
-FROM employees
-WHERE salary % 10000 = 0;
+select first_name,salary from employees where salary % 10000 = 0;
 
