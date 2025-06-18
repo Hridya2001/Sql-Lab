bbad3cf2e4856892df572c662647494b87d91db8
Hridya Umesh <hridyaumesh2@gmail.com>
2025-06-18 15:13:52 +0530

    Update Select.sql

--- Queries/Select.sql
+++ Queries/Select.sql
@@ -1,13 +1,14 @@
--- Select all employees
+
 SELECT * FROM employees;
+-- Select all employees
 
--- Select only first and last names of employees
+
 SELECT first_name, last_name FROM employees;
+-- Select only first and last names of employees
 
--- List all employees earning more than ₹80,000
 SELECT first_name, salary FROM employees
 WHERE salary > 80000;
+-- List all employees earning more than ₹80,000
 
--- Get all departments
 SELECT * FROM departments;
-
+-- Get all departments
