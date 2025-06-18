e9e1adaaabbc38332141d4d8cb723e8826f8f12a
Hridya Umesh <hridyaumesh2@gmail.com>
2025-06-18 15:16:07 +0530

    Update Aggregation.sql

--- Queries/Aggregation.sql
+++ Queries/Aggregation.sql
@@ -1,19 +1,19 @@
--- 1. Count total number of employees
+-- (Count total number of employees)
 SELECT COUNT(*) AS total_employees FROM employees;
 
--- 2. Total salary paid to all employees
+-- (Total salary paid to all employees)
 SELECT SUM(salary) AS total_payroll FROM employees;
 
--- 3. Average salary
+-- (Average salary)
 SELECT AVG(salary) AS average_salary FROM employees;
 
--- 4. Lowest salary
+-- (Lowest salary)
 SELECT MIN(salary) AS lowest_salary FROM employees;
 
--- 5. Highest salary
+-- (Highest salary)
 SELECT MAX(salary) AS highest_salary FROM employees;
 
--- 6. Total salary by department
+-- (Total salary by department)
 SELECT department_id, SUM(salary) AS dept_salary
 FROM employees
 GROUP BY department_id;
