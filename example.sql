0e273c1a3cd9ee034ec0bf1bb6eb9f1270255a51
Hridya Umesh <hridyaumesh2@gmail.com>
2025-06-21 19:15:26 +0530

    Update example.sql

--- example.sql
+++ example.sql
@@ -1,1 +1,11 @@
+-- example.sql
+
+
+CREATE TABLE student (
+    id INT PRIMARY KEY,
+    name VARCHAR(100),
+    grade CHAR(2)
+);
+
+INSERT INTO student VALUES (1, 'Anu', 'A'), (2, 'Ravi', 'B');
 
